5aa4f2726c0cdf29286d3f1086a1605e9c6226cd
Mark Wickham <markjw@uw.edu>
2018-09-18 12:08:33 -0500

    initial commit

---
+++ gen/com/wickham/android/weka_android_3_8_1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wickham.android.weka_android_3_8_1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
